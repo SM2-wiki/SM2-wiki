cbb157d387aa61ee29b9f447d052d2989cfab771
Lv <35390703+Rye-Catcher@users.noreply.github.com>
2019-09-22 12:30:33 +0800

    再次调试page_copyright显示

--- docs/_static/js/extra.js
+++ docs/_static/js/extra.js
@@ -3,20 +3,25 @@ var url = document.getElementsByClassName('page_edit_url')[0].href.split('docs')
 var script = document.createElement('script');
 script.src = `https://api.github.com/repos/SM2-wiki/SM2-wiki/commits?path=docs${url}&callback=foo`;
 var data = script.data;
-if (data && data[0]) {
+/*if (data && data[0]) {
    var ti = new Date(data[0].commit.author.date).toLocaleString('zh-CN', { timeZone: "Asia/Shanghai", hour12: false });
    document.getElementsByClassName('facts_modified')[0].innerHTML = (ti);
-   document.getElementsByClassName('page_contributors')[0].innerHTML = (sorted.join(', '));
 }
 else if (!url.endsWith('index')) {
     url += '/index';
     var script = document.createElement('script');
-     var script = document.createElement('script');
     document.getElementsByClassName('page_info')[0].setAttribute('href', `https://github.com/SM2-wiki/SM2-wiki/blob/master/docs/${url}`);
     document.getElementsByClassName('edit_history')[0].setAttribute('href', `https://github.com/SM2-wiki/SM2-wiki/commits/master/docs/${url}`);
     script.src = `https://api.github.com/repos/SM2-wiki/SM2-wiki/commits?path=docs/${url}&callback=foo`;
     document.getElementsByTagName('head')[0].appendChild(script);
-}
+}*/
+var ti = new Date(data[0].commit.author.date).toLocaleString('zh-CN', { timeZone: "Asia/Shanghai", hour12: false });
+document.getElementsByClassName('facts_modified')[0].innerHTML = (ti);
+var script = document.createElement('script');
+document.getElementsByClassName('page_info')[0].setAttribute('href', `https://github.com/SM2-wiki/SM2-wiki/blob/master/docs/${url}`);
+document.getElementsByClassName('edit_history')[0].setAttribute('href', `https://github.com/SM2-wiki/SM2-wiki/commits/master/docs/${url}`);
+script.src = `https://api.github.com/repos/SM2-wiki/SM2-wiki/commits?path=docs/${url}&callback=foo`;
+document.getElementsByTagName('head')[0].appendChild(script);
 
 if (url == "/index.md") {
   // HomePage
